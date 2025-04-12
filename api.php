8603fdf1fffc5cefc42f7c99f7ac9ce61d57df71
AlexandreLeCam <a.lecam@myanapro.com>
2025-04-12 12:09:27 +0200

    api v1

---
+++ api.php
@@ -0,0 +1,17 @@
+<?php
+// Autorise les appels depuis tous les domaines (ou remplace * par projet5.traefik.me)
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+// Optionnel : autoriser certaines méthodes si tu veux gérer POST plus tard
+header('Access-Control-Allow-Methods: GET');
+
+// Ta logique API habituelle
+$db = require __DIR__ . '/db.php';
+
+$messages = $db->select('demo', ['id', 'message']);
+
+echo json_encode([
+    'success' => true,
+    'data' => $messages,
+]);
