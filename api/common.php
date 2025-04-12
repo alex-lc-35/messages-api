8e9e86fa780e6f40373a56978ca17101f4b10ef2
AlexandreLeCam <a.lecam@myanapro.com>
2025-04-12 15:11:07 +0200

    Access-Control

--- api/common.php
+++ api/common.php
@@ -1,8 +1,18 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type');
+    http_response_code(204);
+    exit;
+}
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
