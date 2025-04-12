94955e97de0a89a87893bce20d4994a80147e3ab
AlexandreLeCam <a.lecam@myanapro.com>
2025-04-12 14:30:43 +0200

    api v2.4

--- database/db.php
+++ database/db.php
@@ -3,12 +3,16 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Medoo\Medoo;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 return new Medoo([
     'type' => 'mysql',
-    'host' => 'sandbox-mysql',
-    'database' => 'sandbox_api',
-    'username' => 'user',
-    'password' => 'password',
-    'charset' => 'utf8mb4'
+    'host' => $_ENV['DB_HOST'],
+    'database' => $_ENV['DB_NAME'],
+    'username' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASS'],
+    'charset' => 'utf8mb4',
 ]);
