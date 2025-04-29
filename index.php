16cf088fb0567ab583fa4735d08997976a0f0dd7
AlexandreLeCam <a.lecam@myanapro.com>
2025-04-29 18:04:52 +0200

    update index

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ $db = require __DIR__ . '/database/db.php';
 $messages = $db->select('messages', ['id', 'content']);
 
 $res = "";
-foreach ($messages as $user) {
-    $res .= $user['id'] . "\t" . $user['content'] . "\n";
+foreach ($messages as $message) {
+    $res .= $message['id'] . "\t" . $message['content'] . "\n";
 }
 
 echo nl2br($res);
